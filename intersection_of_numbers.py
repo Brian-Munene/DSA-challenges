f4a45f7cb4c74cdb82c4bbffc7a58882e41baf2d
Brian-Munene <brianmunene69@gmail.com>
2022-09-09 15:44:22 +0300

    adds solution for intersection of numbers question

--- intersection_of_numbers.py
+++ intersection_of_numbers.py
@@ -9,4 +9,16 @@ def intersection_of_numbers(nums1: list, nums2: list)-> list:
     nums1 = [1, 2, 3, 3], nums2 = [3, 3], return [3]
     nums1 = [2, 4, 6, 8], nums2 = [1, 3, 5, 7], return []
     """
+    res = []
     
+    for i in nums1:
+        if i in nums2 and i not in res:
+            res.append(i)
+    return res
+
+if __name__ == "__main__":
+    vals = [[[2, 4, 4, 2], [2, 4]], [[1, 2, 3, 3], [3, 3]], [[2, 4, 6, 8], [1, 3, 5, 7]]]
+
+    for val in vals:
+        print(intersection_of_numbers(nums1=val[0], nums2=val[1]))
+
